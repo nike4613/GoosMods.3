44a8afa84ca77ef62857778898c4c8a37e3d9b9e
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-02-11 18:12:40 -0600

    Add project files.

---
+++ HatGoos/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatGoos
+{
+    class Config
+    {
+        public enum HatType
+        { 
+            Default, 
+            None, 
+            Custom 
+        }
+        public HatType HatMode = HatType.Default;
+        public float HorizontalSize = 1.5f;
+        public float HatPosition = .6f;
+        public string CustomHatPath = "";
+    }
+}
---
+++ HatGoos/HatGoosMod.cs
@@ -0,0 +1,58 @@
+﻿using GooseShared;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nett;
+using System.IO;
+
+namespace HatGoos
+{
+    public class HatGoosMod : IMod
+    {
+        private Config config = null;
+        private Bitmap hatImage = null;
+
+        public void Init()
+        {
+            InjectionPoints.PostModsLoaded += PostModsLoaded;
+            InjectionPoints.PostRenderEvent += PostRenderEvent;
+        }
+
+        private void PostModsLoaded()
+        {
+            // load configs
+            var path = Path.Combine(API.Helper.getModDirectory(this), "config.toml");
+            if (File.Exists(path))
+                config = Toml.ReadFile<Config>(path);
+            else
+                config = new Config();
+            Toml.WriteFile(config, path);
+
+            // initialize hat image
+            switch (config.HatMode)
+            {
+                case Config.HatType.None:
+                    hatImage = new Bitmap(1, 1);
+                    hatImage.SetPixel(0, 0, Color.Transparent);
+                    break;
+                case Config.HatType.Default:
+                    hatImage = Resources.Default;
+                    break;
+                case Config.HatType.Custom:
+                    hatImage = new Bitmap(Config.CustomHatPath);
+                    break;
+            }
+        }
+
+        private void PostRenderEvent(GooseEntity goose, Graphics g)
+        {
+            // I can't actually do anything useful per goose entity, because there isn't a way to
+            //   register info when a goose entity is created
+
+
+        }
+    }
+}
